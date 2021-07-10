af6d5d12996b8e62aa3fb04b033cdb25c5ebcde4
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-11 03:33:44 +0600

    negative timeing solutation

--- app.js
+++ app.js
@@ -68,20 +68,34 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
-  sliders.forEach(slide => {
-    let item = document.createElement('div')
-    item.className = "slider-item";
-    item.innerHTML = `<img class="w-100"
-    src="${slide}"
-    alt="">`;
-    sliderContainer.appendChild(item)
-  })
-  changeSlide(0)
-  timer = setInterval(function () {
-    slideIndex++;
-    changeSlide(slideIndex);
-  }, duration);
-}
+
+  if(duration < 0){
+    alert("duration can not be negative")
+    document.querySelector('.main').style.display = 'none';
+    imagesArea.style.display = 'block';
+    document.getElementById('duration').value = -duration;
+    return;
+    
+  }
+
+  else{
+
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item)
+    })
+    changeSlide(0)
+    timer = setInterval(function () {
+      slideIndex++;
+      changeSlide(slideIndex);
+    }, duration);
+  }
+  }
+  
 
 // change slider index 
 const changeItem = index => {
